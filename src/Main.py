94f828796422c232aed7074f33180aa1bb2075f5
Assaf Sapir <meijin007@gmail.com>
2017-09-06 21:20:06 +0300

    Add main, fix Person

--- src/Main.py
+++ src/Main.py
@@ -23,8 +23,28 @@ def find_heir_downward(person):
 
 
 def find_heir(person):
+    """
+
+    :type person: Person
+    """
     heir = find_heir_downward(person)
     if heir is not None:
         return heir
 
     return find_heir(person.get_parent())
+
+
+if __name__ == '__main__':
+    def main():
+        robb = Person("Robb", None, None, False)
+        bran = Person("Brandon", None, None, True)
+        rickon = Person("Rickon", None, None, False)
+        sansa = Person("Sansa", None, None, True)
+        arya = Person("Arya", None, None, True)
+        jon = Person("Jon", None, None, True)
+
+        ned = Person("Eddard", [robb, bran, rickon, jon], [sansa, arya], False)
+        for child in ned.get_children:
+            child.set_parent(ned)
+
+        print find_heir(ned)
--- src/Person.py
+++ src/Person.py
@@ -1,12 +1,19 @@
 class Person:
-    def __init__(self, name, parent, sons=[], daughters=[]):
+    def __init__(self, name, sons=[], daughters=[], is_alive=True):
         self.mName = name
-        self.mParent = parent
         self.mSons = sons
         self.mDaughters = daughters
+        self.mParent = None
+        self.isAlive = is_alive
 
     def __str__(self):
         return self.mName
+
+    def set_parent(self, parent):
+        self.mParent = parent
+
+    def get_parent(self):
+        return self.mParent
 
     def get_sons(self):
         return self.mSons
@@ -14,11 +21,17 @@ class Person:
     def get_daughters(self):
         return self.mDaughters
 
-    def get_parent(self):
-        return self.mParent
+    def get_children(self):
+        return self.mSons + self.mDaughters
 
     def get_name(self):
         return self.mName
 
     def is_alive(self):
-        return True
+        return self.isAlive
+
+    def kill(self):
+        self.isAlive = False
+
+    def fire_magic(self):
+        self.isAlive = True
